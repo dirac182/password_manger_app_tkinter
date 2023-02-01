190e38b4d5617e563c8df7dd1068e0d0003157b5
dirac182 <dmorin9696@yahoo.com>
2023-02-01 16:01:19 -0600

    Fixed the UI and Generator. Just need file updates.

--- main.py
+++ main.py
@@ -2,37 +2,36 @@
 import random
 import string
 
-def generator():
 
-    if pass_vars.get == 1:
-        letters = letters_spin.get()
-        numbers = numbers_spin.get()
-        symbols = symbols_spin.get()
+
+def spin_values():
+
+    if pass_vars.get() == 1:
+        LETTERS = int(letters_spin.get())
+        NUMBERS = int(numbers_spin.get())
+        SYMBOLS = int(symbols_spin.get())
     else:
-        letters = 6
-        numbers = 2
-        symbols = 0
+        LETTERS = 6
+        NUMBERS = 2
+        SYMBOLS = 0
+    generator(LETTERS, NUMBERS, SYMBOLS)
+
+def generator(let,numbers,sym):
 
     alpha = list(string.ascii_letters)
     nums = list(string.digits)
     syms = list(string.punctuation)
-
     main_list = []
-
-    for num in range(letters):
+    for num in range(let):
         alpha_list = random.choice(alpha)
         main_list.append(alpha_list)
-
     for num in range(numbers):
         num_list = random.choice(nums)
         main_list.append(num_list)
-
-    for num in range(symbols):
+    for num in range(sym):
         sym_list = random.choice(syms)
         main_list.append(sym_list)
-
     random.shuffle(main_list)
-
     new_pass = ''.join(map(str, main_list))
     pass_entry.delete(0,"end")
     pass_entry.insert(0,new_pass)
@@ -56,6 +55,12 @@ def checkbox_clicked():
         numbers_spin.grid(column=1, row=5,sticky="e")
         symbols_label.grid(column=2, row=5)
         symbols_spin.grid(column=3, row=5,sticky="e")
+        let_var = IntVar(window)
+        num_var = IntVar(window)
+        let_var.set(6)
+        num_var.set(2)
+        letters_spin.config(textvariable=let_var)
+        numbers_spin.config(textvariable=num_var)
     else:
         letters_label.grid_forget()
         letters_spin.grid_forget()
@@ -90,7 +95,7 @@ pass_label.grid(column=0,row=3,sticky="e")
 pass_entry = Entry(width=31,bg=WHITE,fg=BLACK)
 pass_entry.grid(column=1,row=3,sticky="NW")
 
-pass_button = Button(text="Generate Password",bg=WHITE,fg=BLACK,command=generator)
+pass_button = Button(text="Generate Password",bg=WHITE,fg=BLACK,command=spin_values)
 pass_button.grid(column=2,row=3,sticky="NW")
 
 save_button = Button(text="Save",bg=WHITE,fg=BLACK,width=50)
