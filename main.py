86cd97898150861f50bc3e805fc85a61a6658a41
dirac182 <dmorin9696@yahoo.com>
2023-02-01 01:06:36 -0600

    Added the Generator, trying to allow spins to change generator values

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+import random
+import string
 
+def generator():
+
+    if pass_vars.get == 1:
+        letters = letters_spin.get()
+        numbers = numbers_spin.get()
+        symbols = symbols_spin.get()
+    else:
+        letters = 6
+        numbers = 2
+        symbols = 0
+
+    alpha = list(string.ascii_letters)
+    nums = list(string.digits)
+    syms = list(string.punctuation)
+
+    main_list = []
+
+    for num in range(letters):
+        alpha_list = random.choice(alpha)
+        main_list.append(alpha_list)
+
+    for num in range(numbers):
+        num_list = random.choice(nums)
+        main_list.append(num_list)
+
+    for num in range(symbols):
+        sym_list = random.choice(syms)
+        main_list.append(sym_list)
+
+    random.shuffle(main_list)
+
+    new_pass = ''.join(map(str, main_list))
+    pass_entry.delete(0,"end")
+    pass_entry.insert(0,new_pass)
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -12,9 +48,26 @@ LIGHT_BLUE = "#A5C9CA"
 WHITE = "#E7F6F2"
 FONT_NAME = "Courier"
 
+def checkbox_clicked():
+    if pass_vars.get() == 1:
+        letters_label.grid(column=0,row=5,sticky="e")
+        letters_spin.grid(column=1, row=5,sticky="w")
+        numbers_label.grid(column=1, row=5)
+        numbers_spin.grid(column=1, row=5,sticky="e")
+        symbols_label.grid(column=2, row=5)
+        symbols_spin.grid(column=3, row=5,sticky="e")
+    else:
+        letters_label.grid_forget()
+        letters_spin.grid_forget()
+        numbers_label.grid_forget()
+        numbers_spin.grid_forget()
+        symbols_label.grid_forget()
+        symbols_spin.grid_forget()
+
+
 window = Tk()
 window.title("Password Manager")
-window.minsize(width=200,height=200)
+window.minsize(width=600,height=400)
 window.config(padx=20,pady=20,bg=BLACK)
 
 image = PhotoImage(file="logo.png")
@@ -37,17 +90,25 @@ pass_label.grid(column=0,row=3,sticky="e")
 pass_entry = Entry(width=31,bg=WHITE,fg=BLACK)
 pass_entry.grid(column=1,row=3,sticky="NW")
 
-pass_button = Button(text="Generate Password",bg=WHITE,fg=BLACK)
+pass_button = Button(text="Generate Password",bg=WHITE,fg=BLACK,command=generator)
 pass_button.grid(column=2,row=3,sticky="NW")
 
-def checkbox_clicked():
-    if pass_vars.get() == 1:
-        print("Checkbox selected")
-    else:
-        print("Checkbox deselected")
+save_button = Button(text="Save",bg=WHITE,fg=BLACK,width=50)
+save_button.grid(column=1 ,row=8,columnspan=2)
+
+#CHECK DOX DROPDOWN
+letters_label = Label(text="Letters",font=(FONT_NAME,12,"bold"),pady=10,fg=LIGHT_BLUE,bg=BLACK)
+letters_spin = Spinbox(from_=0,to=20,width=5)
+
+numbers_label = Label(text="  Numbers",font=(FONT_NAME,12,"bold"),pady=10,fg=LIGHT_BLUE,bg=BLACK,padx=10)
+numbers_spin = Spinbox(from_=0,to=20,width=5)
+
+symbols_label = Label(text="Symbols",font=(FONT_NAME,12,"bold"),pady=10,fg=LIGHT_BLUE,bg=BLACK)
+symbols_spin = Spinbox(from_=0,to=20,width=5)
 
 pass_vars = IntVar()
-pass_checkbox = Checkbutton(text="Password Parameters",command=checkbox_clicked(),variable=pass_vars,bg=BLACK,fg=WHITE)
+pass_checkbox = Checkbutton(text="Password Parameters",command=checkbox_clicked,variable=pass_vars,bg=BLACK,fg=WHITE)
 pass_checkbox.grid(column=0,row=4)
+
 
 window.mainloop()
